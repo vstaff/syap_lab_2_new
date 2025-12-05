039f97afc229aa0763841f6932352609525a0fae
vstaff <vstaff22b@gmail.com>
2025-12-05 13:45:45 +1000

    создал отдельный файл main.py - здесь главный скрипт который проверяет работу 'транслятора'

---
+++ main.py
@@ -0,0 +1,11 @@
+from translation import translate
+
+
+def main() -> None:
+    translate("./script_1.js")
+    translate("./script_2.js")
+    translate("./script_3.js")
+
+
+if __name__ == '__main__':
+    main()
